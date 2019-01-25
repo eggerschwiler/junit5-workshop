61d25bff50b34c04ea2a443cfe832438523d8a51
David Eggerschwiler <mister_x4@hotmail.com>
2019-01-25 15:17:14 +0100

    Extensions should not have state, thus changed the example to use Parameter Injection and the ExtensionContext.Store.

--- junit5/src/test/java/com/zuehlke/testing/extension/PersonResource.java
+++ junit5/src/test/java/com/zuehlke/testing/extension/PersonResource.java
@@ -2,26 +2,23 @@ package com.zuehlke.testing.extension;
 
 import com.zuehlke.testing.Person;
 import com.zuehlke.testing.PersonDao;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PersonResource implements AfterEachCallback {
+public class PersonResource {
 
+	private List<Person> people = new ArrayList<>();
 	private PersonDao dao = new PersonDao();
-	private Collection<Person> people = new LinkedList<>();
 
-	Person createPerson(String name) {
+	public Person createPerson(String name) {
 		final Person person = new Person("", name);
 		dao.save(person);
 		people.add(person);
 		return person;
 	}
 
-	@Override
-	public void afterEach(ExtensionContext context) throws Exception {
+	void cleanUp() {
 		for (Person person : people) {
 			dao.delete(person);
 		}
