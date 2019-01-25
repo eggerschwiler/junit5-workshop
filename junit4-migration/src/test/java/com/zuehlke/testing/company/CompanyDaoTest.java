56cc253499d7f3b734b4d00d98508ded7af93170
David Eggerschwiler <mister_x4@hotmail.com>
2019-01-25 15:32:38 +0100

    Extensions should not have state, thus changed the example to use Parameter Injection and the ExtensionContext.Store.

--- junit4-migration/src/test/java/com/zuehlke/testing/company/CompanyDaoTest.java
+++ junit4-migration/src/test/java/com/zuehlke/testing/company/CompanyDaoTest.java
@@ -2,22 +2,20 @@ package com.zuehlke.testing.company;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
 
 import java.util.Collection;
 import java.util.LinkedList;
 
+@ExtendWith(CompanyDaoTest.CompanyResourceExtension.class)
 class CompanyDaoTest {
 
     private CompanyDao dao = new CompanyDao();
 
-    @RegisterExtension
-    CompanyResource companyResource = new CompanyResource();
-
     @Test
-    void find() {
+    void find(CompanyResource companyResource) {
         // arrange
         Company expected = companyResource.createCompany("Zuehlke Engineering AG");
         // act
@@ -26,7 +24,39 @@ class CompanyDaoTest {
         Assertions.assertEquals(expected.getName(), result.getName());
     }
 
-    private class CompanyResource implements AfterEachCallback {
+    static class CompanyResourceExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
+
+        private static final String COMPANY_RESOURCE = "companies";
+        private final Namespace namespace = Namespace.create("Zuehlke", "Testing", "Companies");
+
+        @Override
+        public void beforeEach(ExtensionContext context)  {
+            getStore(context).put(COMPANY_RESOURCE, new CompanyResource());
+        }
+
+
+        @Override
+        public void afterEach(ExtensionContext context) {
+            CompanyResource companyResource = (CompanyResource) getStore(context).get(COMPANY_RESOURCE);
+            companyResource.cleanUp();
+        }
+
+        @Override
+        public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+            return CompanyResource.class.equals(parameterContext.getParameter().getType());
+        }
+
+        @Override
+        public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+            return getStore(extensionContext).get(COMPANY_RESOURCE);
+        }
+
+        private Store getStore(ExtensionContext context) {
+          return context.getStore(namespace);
+        }
+    }
+
+    private static class CompanyResource {
 
         private CompanyDao dao = new CompanyDao();
         private Collection<Company> companies = new LinkedList<Company>();
@@ -38,8 +68,7 @@ class CompanyDaoTest {
             return company;
         }
 
-        @Override
-        public void afterEach(ExtensionContext context) throws Exception {
+        public void cleanUp() {
             for (Company company : companies) {
                 dao.delete(company);
             }
