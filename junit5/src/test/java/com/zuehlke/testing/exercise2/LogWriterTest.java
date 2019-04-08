3f1b415ee6add25a87a94f60d545c1289354b1df
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-08 16:25:41 +0300

    Merge branch 'master' into solutions Add solutions for new exercises.

---
+++ junit5/src/test/java/com/zuehlke/testing/exercise2/LogWriterTest.java
@@ -0,0 +1,29 @@
+package com.zuehlke.testing.exercise2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+class LogWriterTest {
+
+    private LogWriter testee = new LogWriter();
+
+    @Test
+    void log(@TempDir Path tempDir) throws IOException {
+        // arrange
+        Path filePath = tempDir.resolve("logFile.txt");
+        File logFile = new File(filePath.toUri());
+        // act
+        testee.log(logFile, "Hello Log");
+        // assert
+        String result = new String(Files.readAllBytes(logFile.toPath()));
+        assertThat(result, containsString("Hello Log"));
+    }
+}
---
+++ junit5/src/test/java/com/zuehlke/testing/solution5/AnIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.zuehlke.testing.solution5;
+
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class AnIntegrationTest {
+
+    @Test
+    void testIntegration() throws InterruptedException {
+        //some long running test (5 Minutes)
+        Thread.sleep(5*60*1000000);
+    }
+}
