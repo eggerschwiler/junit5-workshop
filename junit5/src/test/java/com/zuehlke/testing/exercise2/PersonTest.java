36145dee8fed6b83b808f5623630d404aa1a412c
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-25 14:45:46 +0200

    Add PersonTest needed in Exercise 2

---
+++ junit5/src/test/java/com/zuehlke/testing/exercise2/PersonTest.java
@@ -0,0 +1,35 @@
+package com.zuehlke.testing.exercise2;
+
+import com.zuehlke.testing.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+
+    @Test
+    void testPerson_firstNameGiven() {
+        Person person = new Person("Hans", "Meier");
+        assertEquals("Hans", person.getFirstName(), "first Name");
+        assertEquals("Meier", person.getLastName(), "last Name");
+    }
+
+    @Test
+    void testPerson_firstNameEmpty() {
+        Person person = new Person("", "Meier");
+        assertThat("first name", person.getFirstName(), is(emptyString()));
+        assertThat("last name", person.getLastName(), is(equalTo("Meier")));
+    }
+
+    @Test
+    void testPerson_firstNameNull() {
+        Person person = new Person(null, "Meier");
+        assertThat("first name", person.getFirstName(), is(nullValue()));
+        assertThat("last name", person.getLastName(), is(equalTo("Meier")));
+    }
+}
